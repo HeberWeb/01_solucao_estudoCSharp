337cc967023eb32a475ae667b6fb9768a6099b5e
Heber Pietrafeza <hpietrafeza@autodoc.com.br>
2019-04-11 15:13:22 -0300

    Validations em andamento

---
+++ 02_Validacao/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace _02_Validacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Usuario usuario = new Usuario() { Nome = "José", Email = "jose", Senha = "aaaa"};
+            ValidationContext contexto = new ValidationContext(usuario);
+            List<ValidationResult> resultados = new List<ValidationResult>();
+            if(Validator.TryValidateObject(usuario, contexto, resultados, true) == false)
+            {
+                //Mensagens
+                foreach (var erro in resultados)
+                {
+                    Console.WriteLine(erro.ErrorMessage);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
