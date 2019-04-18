75f581e06f146e65a77224c3090496092d555e54
Heber Pietrafeza <hpietrafeza@autodoc.com.br>
2019-04-18 13:12:17 -0300

    Incluindo validationAttrbute propria.

---
+++ 03_ValidacaoCustomizada/Usuario.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace _03_ValidacaoCustomizada
+{
+    class Usuario
+    {
+        [Required(ErrorMessage = "O campo nome é de preenchimento obrigatório!")]
+        public string Nome { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [MyValidationAttribute(ErrorMessage = "O campo senha deve possuir 10 caracteres.")]
+        public string Senha { get; set; }
+    }
+}
